2db13d7cac2628e167ab90f0c6f5d7b86d8aba33
Nrion Fx <nrion.fx@gmail.com>
2013-03-10 18:09:33 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberGbs::Application.config.secret_token = '53f9eb35df765979dc60eaa249712866eb74b30032f9c52c17052950962d3432b5462ec84fe8130984b9f509aa9a28443ce44c0d1d134af81093a3d27fdef87d'
